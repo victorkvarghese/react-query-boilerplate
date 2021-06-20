3bc6cd7f08a4f6658897ab7346cc3e93aad2cf45
Victor-Varghese <victor.varghese@neudesic.com>
2021-06-20 09:12:53 +0530

    axios interceptors for headers/token update from session

--- src/services/clients/api-client.ts
+++ src/services/clients/api-client.ts
@@ -6,3 +6,24 @@ export const apiClient = axios.create({
   responseType: 'json',
   withCredentials: false,
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const updatedConfig = {
+      ...config,
+    };
+
+    // TODO: ADd TOKEN or headers here for each api call
+    // get token from session storage if needed
+    // const { token } = store.getState().session;
+    const token = '';
+
+    updatedConfig.headers.Authorization = `Bearer ${token}`;
+    console.log('updatedConfig', updatedConfig);
+
+    return updatedConfig;
+  },
+  (error) =>
+    // Do something with request error
+    Promise.reject(error)
+);
